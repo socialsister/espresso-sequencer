c48ccea2f89ed106ff1f516fb2df16dc403d46f7
socialsister <socialsister@users.noreply.github.com>
2026-03-30 01:17:20 +0000

    deploy: 28449cb4dd3b1c35f757e9e77b26546b0e9fdb76

--- trait.impl/core/convert/trait.AsMut.js
+++ trait.impl/core/convert/trait.AsMut.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["hotshot_query_service",[["impl&lt;D, U&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.0/core/convert/trait.AsMut.html\" title=\"trait core::convert::AsMut\">AsMut</a>&lt;U&gt; for <a class=\"struct\" href=\"hotshot_query_service/data_source/struct.ExtensibleDataSource.html\" title=\"struct hotshot_query_service::data_source::ExtensibleDataSource\">ExtensibleDataSource</a>&lt;D, U&gt;",0]]]]);
+    const implementors = Object.fromEntries([["hotshot_query_service",[["impl&lt;D, U&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/convert/trait.AsMut.html\" title=\"trait core::convert::AsMut\">AsMut</a>&lt;U&gt; for <a class=\"struct\" href=\"hotshot_query_service/data_source/struct.ExtensibleDataSource.html\" title=\"struct hotshot_query_service::data_source::ExtensibleDataSource\">ExtensibleDataSource</a>&lt;D, U&gt;",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
