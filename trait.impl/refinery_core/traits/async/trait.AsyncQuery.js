a65a535551b5e8d4272b6a0ccbe109284f27d8d6
socialsister <socialsister@users.noreply.github.com>
2026-04-20 01:23:51 +0000

    deploy: 28449cb4dd3b1c35f757e9e77b26546b0e9fdb76

--- trait.impl/refinery_core/traits/async/trait.AsyncQuery.js
+++ trait.impl/refinery_core/traits/async/trait.AsyncQuery.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["hotshot_query_service",[["impl AsyncQuery&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/1.94.1/alloc/vec/struct.Vec.html\" title=\"struct alloc::vec::Vec\">Vec</a>&lt;<a class=\"struct\" href=\"hotshot_query_service/data_source/sql/struct.Migration.html\" title=\"struct hotshot_query_service::data_source::sql::Migration\">Migration</a>&gt;&gt; for <a class=\"struct\" href=\"hotshot_query_service/data_source/storage/sql/migrate/struct.Migrator.html\" title=\"struct hotshot_query_service::data_source::storage::sql::migrate::Migrator\">Migrator</a>&lt;'_&gt;",0]]]]);
+    const implementors = Object.fromEntries([["hotshot_query_service",[["impl AsyncQuery&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/1.95.0/alloc/vec/struct.Vec.html\" title=\"struct alloc::vec::Vec\">Vec</a>&lt;<a class=\"struct\" href=\"hotshot_query_service/data_source/sql/struct.Migration.html\" title=\"struct hotshot_query_service::data_source::sql::Migration\">Migration</a>&gt;&gt; for <a class=\"struct\" href=\"hotshot_query_service/data_source/storage/sql/migrate/struct.Migrator.html\" title=\"struct hotshot_query_service::data_source::storage::sql::migrate::Migrator\">Migrator</a>&lt;'_&gt;",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
